b651ff723aed14f99258e101b50f45d95e7a1faa
Andreas Gabriel <gandev.de@gmail.com>
2013-12-02 00:39:22 +0100

    remove experimental features

--- helpers.js
+++ helpers.js
@@ -1,11 +1,4 @@
-var glob = Npm.require('glob');
 var path = Npm.require('path');
-
-//js-git
-var platform = Npm.require('git-node-platform');
-var jsGit = Npm.require('js-git');
-var fsDb = Npm.require('git-fs-db')(platform);
-var fs = platform.fs;
 
 var project_path = path.join(process.cwd(), '..', '..', '..', '..', '..');
 
@@ -34,125 +27,12 @@ updateMetadata = function() {
   });
 };
 
-var createFileTreeSync = function(dir) {
-  var file_tree = {};
-  var files = glob.sync(dir, {
-    cwd: project_path
-  });
-
-  var addToResult = function(file, path) {
-    if (path.length === 0) {
-      file_tree[file] = 'file';
-    }
-
-    var obj = file_tree;
-    for (var i = 0; i < path.length; i++) {
-      var key = path[i];
-      if (_.isEmpty(key)) continue;
-
-      if (!(key in obj) || _.isString(obj[key])) {
-        obj[key] = {};
-      }
-
-      obj = obj[key];
-
-      if (path.length - 1 === i) {
-        obj[file] = 'file';
-      }
-    }
-  };
-
-  for (var i = 0; i < files.length; i++) {
-    var file_path = files[i].split(path.sep);
-    var file = file_path.pop();
-
-    addToResult(file, file_path);
-  }
-  return file_tree;
-};
-
-var createGitTree = function(hash, callback) {
-  var git_tree = {};
-
-  // Create a filesystem backed bare repo
-  var repo = jsGit(fsDb(fs(project_path + '/.git')));
-  repo.logWalk(hash, function(err, log) {
-    if (err) throw err;
-    var shallow;
-
-    var onRead = function(err, commit) {
-      if (err) throw err;
-      if (!commit) return logEnd(shallow);
-      if (commit.last) shallow = true;
-      var commit_date = addCommit(commit);
-      repo.treeWalk(commit.tree, function(err, tree) {
-        if (err) throw err;
-
-        var onEntry = function(err, entry) {
-          if (err) throw err;
-
-          if (!entry) {
-            return log.read(onRead);
-          }
-          addEntry(entry, commit_date);
-          return tree.read(onEntry);
-        };
-
-        tree.read(onEntry);
-      });
-    };
-
-    return log.read(onRead);
-  });
-
-  var addCommit = function(commit) {
-    var author = commit.author.name;
-    var message = commit.message;
-    var date = commit.author.date.toString();
-
-    git_tree[date] = {
-      author: author,
-      message: message
-    };
-
-    return date;
-  };
-
-  var addEntry = function(entry, commit_date) {
-    var path = entry.path;
-    var hash = entry.hash;
-    var entry_obj = {
-      path: path,
-      hash: hash
-    };
-
-
-    if (git_tree[commit_date].entries) {
-      git_tree[commit_date].entries.push(entry_obj);
-    } else {
-      git_tree[commit_date].entries = [entry_obj];
-    }
-  };
-
-  var logEnd = function(shallow) {
-    var message = shallow ? "End of shallow record." : "Beginning of history";
-
-    if (typeof callback === 'function') {
-      callback.call(null, git_tree);
-    }
-  };
-};
-
 //helper definitions
-
-ServerEval.helpers.listFiles = function(callback, path) {
-  return createFileTreeSync(path);
-};
 
 ServerEval.helpers.updateMetadata = function() {
   updateMetadata();
 };
 
-ServerEval.helpers.gitLog = function(callback, hash) {
-  createGitTree(hash || 'HEAD', callback);
+ServerEval.helpers.gitStatus = function(callback, hash) {
+
 };
--- package.js
+++ package.js
@@ -1,11 +1,3 @@
-Npm.depends({
-  'glob': '3.2.7',
-  'js-git': '0.6.1',
-  'git-node-platform': '0.1.4',
-  'git-fs-db': '0.2.0',
-  'git-wrapper': '0.1.1'
-});
-
 Package.describe({
   summary: "allows client to run js in server context"
 });
@@ -14,6 +6,8 @@ Package.on_use(function(api) {
   api.use(['standard-app-packages'], ['client', 'server']);
 
   api.export('ServerEval');
+
+  api.add_files('lib/git.js', 'server');
 
   api.add_files('result_format.js', 'server');
   api.add_files('eval.js', ['client', 'server']);
